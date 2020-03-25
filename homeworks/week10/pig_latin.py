0d7692e2966ab884f59c2cc4f4fc7e83c41d80e2
Coral Sheldon-Hess <ha@haha.nope>
2020-03-25 16:55:38 -0400

    lots changed

--- homeworks/week10/pig_latin.py
+++ homeworks/week10/pig_latin.py
@@ -9,26 +9,25 @@ import os
 import string
 
 
-def main():
-    # get the text from the file
-    prejudice = get_text('prejudice.txt')
-    
-    # prove to ourselves that it worked
-    print(word_list)
-
-
-# get the text from the specified file
 # you do not need to change this function
 # (but if you want to, you can)
 def get_text(filename):
+    """get the text from the specified file, with its path passed in as a string"""
     the_text = ""
     # is the file actually there?
     if os.path.isfile(filename):
         # open it, and read its contents
         file_handler = open(filename, 'r')
         the_text = file_handler.read()
-
     return the_text
+
+
+def main():
+    # get the text from the file
+    prejudice = get_text('prejudice.txt')
+    
+    # prove to ourselves that it worked
+    print(prejudice)
 
 
 # the incantation to make the program go
